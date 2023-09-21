9285210365f2460c85c41c281fcb232f532074d3
Hatef Barin <amir.barin76@gmail.com>
2023-09-21 13:47:03 +0430

    Add doctest

---
+++ 01_doctest.py
@@ -0,0 +1,36 @@
+"""
+>>> add(7, 6)
+13
+>>> add(4, -1)
+3
+"""
+
+
+def add(x, y):
+    """
+    >>> add(7, 6)
+    13
+    >>> add(4, -1)
+    3
+    """
+    return x + y
+
+
+def subtract(x, y):
+    """
+    >>> subtract(7, 6)
+    1
+    >>> subtract(4, -1)
+    5
+    """
+    return x - y
+
+
+def multiply(x, y):
+    """
+    >>> multiply(7, 6)
+    42
+    >>> multiply(4, -1)
+    -4
+    """
+    return x * y
